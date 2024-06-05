83d0b0dadd9b7f828e862deadd34572cb24ce58c
NOMAN1802 <mustakimnoman82@gmail.com>
2024-06-05 20:57:12 +0600

    academic faculties

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,17 +16,29 @@ export  const findLastStudentId = async() =>{
     })
     .lean()
 
-    return lastStudent?.id  ? lastStudent.id.substring(6) : undefined;
+    return lastStudent?.id  ? lastStudent.id : undefined;
 
 };
 
 
-
 export const generateStudentId = async (payload: TAcademicSemester) =>{
 
-    // first time
+    // first time 0000
+    // 0001 --> 1
+    let currentId =  (0).toString(); //default --> 0000
 
-    const currentId = await findLastStudentId() || (0).toString();
+    const lastStudentId = await findLastStudentId();
+    // 2030 01 0001
+    const lastStudentSemesterCode = lastStudentId?.substring(4,6); //01
+    const lastStudentYear = lastStudentId?.substring(0,4) //2030
+
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if(lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentYear ){
+
+       currentId = lastStudentId.substring(6); //0001
+    }
 
     let incrementId = (Number(currentId)+1).toString().padStart(4,'0');
 
