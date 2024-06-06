9ffd9b35a4cd68d0e3fd9ac6021d5e466a22f8d8
NOMAN1802 <mustakimnoman82@gmail.com>
2024-06-07 02:23:12 +0600

    error handle zod & mongoose

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,17 +1,61 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-undef*/
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import  { Request, Response, NextFunction } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
-const globalErrorHandler=(err: any, req: Request, res: Response,next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Some thing went wrong' ;
+import  {  ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
+import handleZodError from '../errors/handelZodError';
+import handleValidationError from '../errors/handleValidationError';
+
+const globalErrorHandler:ErrorRequestHandler =(err,req, res,next) => {
+
+  // setting default values
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Some thing went wrong';   
+
+    let errorSources : TErrorSources = [{
+       path: '',
+       message: 'Some thing went wrong',
+    },
+  ];
+
+    if(err instanceof ZodError){
+      
+      const simplifiedError = handleZodError(err)
+      statusCode = simplifiedError?.statusCode;
+      message = simplifiedError?.message;
+      errorSources = simplifiedError?.errorSources;
+    }else if(err?.name === 'ValidationError'){
+      
+      const simplifiedError = handleValidationError(err)
+      statusCode = simplifiedError.statusCode;
+      message = simplifiedError.message;
+      errorSources = simplifiedError.errorSources;
+    }
+
+    //ultimate return
+
     return res.status(statusCode).json({
       success: false,
       message,
-      error: err,
+      errorSources,
+      // err,
+      stack: config.NODE_ENV === 'development' ? err?.stack : null,
+      
     })
   };
 
   export default globalErrorHandler;
+
+  /*   
+   success
+   message
+   errorSources:[
+   path:'',
+   message: ''
+
+   ]
+   stack
+  */
